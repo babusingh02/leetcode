2ad8cc1def4a6c0551e84c382fb78641d340b68e
babusingh02 <150677759+babusingh02@users.noreply.github.com>
2025-03-07 10:56:10 +0530

    Time: 58 ms (25.48%), Space: 23.1 MB (44.27%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -0,0 +1,14 @@
+class RecentCounter:
+    TIME_WINDOW = 3_000
+
+    def __init__(self):
+        self.data = deque()
+
+    def ping(self, t: int) -> int:
+        self.data.append(t)
+
+        # remove all calls older than TIME_WINDOW
+        while self.data[0] < t - self.TIME_WINDOW:
+            self.data.popleft()
+        
+        return len(self.data)
